3fef00a8aa1cb60610bd454e5c854d819d35f112
wmmnola <wmmnola@gmail.com>
2017-01-26 16:24:37 -0600

    Some changes

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function draw() {
       if (!(circles[j].x == circles[i].x) && !(circles[j].y == circles[i].x)) {
         var d = dist(circles[i].x, circles[i].y, circles[j].x, circles[j].y);
 
-        if (d <= (circles[i].r / 2 + circles[j].r / 2)) {
+        if (d < (circles[i].r / 2 + circles[j].r / 2)) {
           circles[i].growing = false;
           circles[j].growing = false
         }
