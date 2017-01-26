746a2735f5508cfdb92643916907bde08c8d03ed
wmmnola <wmmnola@gmail.com>
2017-01-26 16:41:33 -0600

    Added basic functionality

--- sketch.js
+++ sketch.js
@@ -5,17 +5,17 @@ function setup() {
   offset = random(windowWidth);
   colorMode(HSB);
   noStroke();
+  var circle = new Circle(.01, random(windowWidth), random(windowHeight),
+    random(1000));
+  circles.push(circle);
 
 }
 
 function draw() {
-  var circle = new Circle(.01,random(windowWidth), random(windowHeight), random(1000) );
-  circles.push(circle);
-  for(var i = 0; i < circles.length; i++){
-    circles[i].draw();
-    if(!circles[i].growing){
-      circles = circles.splice(i);
-    }
+
+  for (var i = 0; i < circles.length; i++) {
+    circles[i].draw(circles);
+
   }
 
 
