d492feb3d20f2fd97482d3cad5ef0ee3b3b99363
wmmnola <wmmnola@gmail.com>
2017-01-25 18:06:59 -0600

    Added lots of circles

--- sketch.js
+++ sketch.js
@@ -1,29 +1,22 @@
-var color = 1;
-var fib = [1, 1];
-var index = 0;
-var x = 0;
-var y = 0;
-var r = 1;
-var acc = 1;
-var offset = 0;
+var circles = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   offset = random(windowWidth);
   colorMode(HSB);
   noStroke();
+
 }
 
 function draw() {
-  if (r < 100 + offset && acc < 100) {
-    ellipse(width / 2, height / 2, r, r);
-    fill(r % 360, 50, color);
-    r += acc;
-    acc += .001;
-    color += .1;
-    console.log(acc);
-  } else {
-    r = 1;
-    offset = random(windowWidth);
+  var circle = new Circle(.01,random(windowWidth), random(windowHeight), random(1000) );
+  circles.push(circle);
+  for(var i = 0; i < circles.length; i++){
+    circles[i].draw();
+    if(!circles[i].growing){
+      circles = circles.splice(i);
+    }
   }
+
+
 }
