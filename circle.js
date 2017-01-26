a10290434afe926a0d1c27a33e7d0ae4017529ca
wmmnola <wmmnola@gmail.com>
2017-01-25 18:07:21 -0600

    Added circle.js

---
+++ circle.js
@@ -0,0 +1,29 @@
+var Circle = function(radius, initx, inity, acc){
+  this.r = radius;
+  this.x = initx;
+  this.y = inity;
+  this.acc = acc/10000;
+  this.vel = 1;
+  this.color = 1;
+  this.bright = random(10,90);
+  this.sat = random(10,90);
+  this.limit = random(500, 1000);
+  this.growing = true;
+
+  this.draw = function(){
+    fill(this.color % 120, this.sat % 100, this.bright % 100, 100);
+    this.color += this.vel
+    if(this.r < this.limit){
+      push();
+      ellipse(this.x, this.y, this.r, this.r)
+      this.r += this.vel;
+      pop();
+    }
+    else{
+      this.growing = false;
+    }
+    this.vel+=this.acc;
+    this.bright += this.acc;
+    this.sat += this.acc;
+  }
+}
