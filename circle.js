b84625b01711aaaf77a7b6f1bc8780b20d525520
wmmnola <wmmnola@gmail.com>
2017-01-25 21:17:25 -0600

    Added functionality such that the circles wouldnt overlap

--- circle.js
+++ circle.js
@@ -1,29 +1,31 @@
-var Circle = function(radius, initx, inity, acc){
+var Circle = function(radius, initx, inity, acc) {
   this.r = radius;
   this.x = initx;
   this.y = inity;
-  this.acc = acc/10000;
+  this.acc = acc / 10000;
   this.vel = 1;
   this.color = 1;
-  this.bright = random(10,90);
-  this.sat = random(10,90);
+  this.bright = random(10, 90);
+  this.sat = random(10, 90);
   this.limit = random(500, 1000);
   this.growing = true;
+  this.kill = false;
 
-  this.draw = function(){
-    fill(this.color % 120, this.sat % 100, this.bright % 100, 100);
-    this.color += this.vel
-    if(this.r < this.limit){
-      push();
-      ellipse(this.x, this.y, this.r, this.r)
-      this.r += this.vel;
-      pop();
+  this.draw = function() {
+    if (this.growing) {
+      fill(this.color % 120, this.sat % 100, this.bright % 100, 100);
+      this.color += this.vel
+      if (this.r < this.limit) {
+        push();
+        ellipse(this.x, this.y, this.r, this.r)
+        this.r += this.vel;
+        pop();
+      } else {
+        this.kill = true;
+      }
+      this.vel += this.acc;
+      this.bright += this.acc;
+      this.sat += this.acc;
     }
-    else{
-      this.growing = false;
-    }
-    this.vel+=this.acc;
-    this.bright += this.acc;
-    this.sat += this.acc;
   }
 }
--- sketch.js
+++ sketch.js
@@ -9,12 +9,23 @@ function setup() {
 }
 
 function draw() {
-  var circle = new Circle(.01,random(windowWidth), random(windowHeight), random(1000) );
+  var circle = new Circle(.01, random(windowWidth), random(windowHeight),
+    random(1000));
   circles.push(circle);
-  for(var i = 0; i < circles.length; i++){
+  for (var i = 0; i < circles.length; i++) {
     circles[i].draw();
-    if(!circles[i].growing){
-      circles = circles.splice(i);
+    for (var j = 0; j < circles.length; j++) {
+      if (!(circles[j].x == circles[i].x) && !(circles[j].y == circles[i].x)) {
+        var d = dist(circles[i].x, circles[i].y, circles[j].x, circles[j].y);
+
+        if (d <= (circles[i].r / 2 + circles[j].r / 2)) {
+          circles[i].growing = false;
+          circles[j].growing = false
+        }
+      }
+      if (circles[i].kill) {
+        circles = circles.splice(i);
+      }
     }
   }
 
